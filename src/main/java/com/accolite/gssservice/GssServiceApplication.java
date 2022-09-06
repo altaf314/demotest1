583d18048f1e678949a3cb7177b3db3da0694793
Altaf <mdaltafraja41@gmail.com>
2022-09-06 13:31:23 +0530

    clean up

--- src/main/java/com/accolite/gssservice/GssServiceApplication.java
+++ src/main/java/com/accolite/gssservice/GssServiceApplication.java
@@ -1,20 +1,13 @@
 package com.accolite.gssservice;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class GssServiceApplication {
-	
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(GssServiceApplication.class, args);
 	}
-
-
 
 }
--- src/main/java/com/accolite/gssservice/controller/FileUploadController.java
+++ src/main/java/com/accolite/gssservice/controller/FileUploadController.java
@@ -1,20 +1,19 @@
 package com.accolite.gssservice.controller;
 
 import java.io.ByteArrayOutputStream;
+
 import org.apache.poi.ss.usermodel.Workbook;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import com.accolite.gssservice.service.CompareSheetUtil;
 
+import com.accolite.gssservice.service.CompareSheetUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,10 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class FileUploadController {
 
-	@Autowired
-	private FileUploadHelper fileUploadHelper;
-
-	
 	@PostMapping("/upload-file")
 	public ResponseEntity<?> uploadFile(@RequestParam("file1") MultipartFile file,
 			@RequestParam("file2") MultipartFile file2) {
@@ -52,29 +47,26 @@ public class FileUploadController {
 				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only Excel File are allowed");
 			}
 
-	
+			Workbook workbook = CompareSheetUtil.compareFile(file, file2);
 
-				Workbook workbook = CompareSheetUtil.compareFile(file, file2);
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			try {
+				workbook.write(bos);
+			} finally {
+				bos.close();
+			}
+			byte[] bytes = bos.toByteArray();
 
+			HttpHeaders header = new HttpHeaders();
+			header.setContentType(
+					new MediaType("application", "vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+			header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=result.xls");
+			header.setContentLength(bytes.length);
 
-				ByteArrayOutputStream bos = new ByteArrayOutputStream();
-				try {
-					workbook.write(bos);
-				} finally {
-					bos.close();
-				}
-				byte[] bytes = bos.toByteArray();
-
-				HttpHeaders header = new HttpHeaders();
-				header.setContentType(
-						new MediaType("application", "vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
-				header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=result.xls");
-				header.setContentLength(bytes.length);
-
-				return ResponseEntity.ok().headers(header).body(bytes);
-
+			return ResponseEntity.ok().headers(header).body(bytes);
 
 		} catch (Exception e) {
+			log.error("Exception occurred :: ", e);
 			e.printStackTrace();
 		}
 
@@ -82,5 +74,4 @@ public class FileUploadController {
 				.body("Some Thing went wrong! Try after Sometime");
 	}
 
-	
 }
